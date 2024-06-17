b2983d1e2707a6c2d9800b53c13bd2f8308a07ba
DosuFE <sdosu@dervac.com>
2024-06-17 23:20:34 +0100

    new editing

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const dark = () => {
+    const hidding = document.querySelector('#image');
+    hidding.hidden= true;
+}
+
+const unHid  = () => {
+    const hidding = document.querySelector('#image');
+    hidding.hidden= false;
+}
